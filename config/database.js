b2fe7b6bc13a6149ae3a8fbe254909cd38c9d69e
Hartwig <hartwig.hii@gmail.com>
2020-11-17 17:03:48 +0200

    db conf update

--- config/database.js
+++ config/database.js
@@ -1,18 +1,4 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'sqlite',
-//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-//       },
-//       options: {
-//         useNullAsDefault: true,
-//       },
-//     },
-//   },
-// });
+if (process.env.DATABASE_URL) {
 
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
@@ -32,6 +18,23 @@ module.exports = ({ env }) => ({
       },
       options: {
         ssl: false,
+      },
+    },
+  },
+});
+} 
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      },
+      options: {
+        useNullAsDefault: true,
       },
     },
   },
